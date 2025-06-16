4f8258ab0f95af7e45528ace3f53b31fce9cc835
suniyafathima <suniyafathima@gmail.com>
2025-06-16 13:11:37 +0530

    a

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        
+  //  ChangeNotifierProvider(create: (_) => MyModel()),
       ],
       child: MaterialApp(
         
