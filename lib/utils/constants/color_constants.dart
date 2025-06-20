942b470bd28ec2f3703776be5d94c11f1cab01f9
suniyafathima <suniyafathima@gmail.com>
2025-06-20 12:05:38 +0530

    product details completed

---
+++ lib/utils/constants/color_constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ColorConstants {
+  static Color primaryColor = Colors.pinkAccent.shade400; 
+  static  Color secondaryColor = Colors.grey.shade400;
+   static  Color grey = Colors.grey.shade800;
+  static Color backgroundColor = Color(0xFFFFFFFF); // White
+  static Color errorColor = Color(0xFFB00020); // Red
+  static Color textColor = Color(0xFF000000); // Black
+}
